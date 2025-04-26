e730962097e9f64aabb7a061a67b08add4d49f17
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-04-26 10:40:28 -0300

    breaking down code

--- study_content/relacoes_end.java
+++ study_content/relacoes_end.java
@@ -1,10 +1,13 @@
+//1- declaração de classes e atributos
 class Endereco {
     private String nome_rua;
     private int numero;
-    public Escola (String nome_rua, int numero){
+//2- construtor
+    public Endereco (String nome_rua, int numero){
         this.setNomeRua(nome_rua);
         this.numero=numero;
     }
+//3- Métodos (setters)
     private void setNomeRua(String nome_rua) {
         this.nome_rua = nome_rua
     }
